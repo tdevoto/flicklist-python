ef5fe5fa09a1955834c4af4a0125e0d6b8621a17
jesseilev <jesseilev@gmail.com>
2016-08-17 16:18:16 -0500

    solve studio5

--- main.py
+++ main.py
@@ -101,19 +101,21 @@ class AddMovie(webapp2.RequestHandler):
         # look inside the request to figure out what the user typed
         new_movie = self.request.get("new-movie")
 
-        # TODO 2
         # if the user typed nothing at all, redirect and yell at them
+        if not new_movie or new_movie.strip() == "":
+            error = "Please specify the movie you want to add."
+            self.redirect("/?error=" + cgi.escape(error))
 
-
-        # TODO 3
         # if the user wants to add a terrible movie, redirect and yell at them
+        if new_movie in terrible_movies:
+            error = "Trust me, you don't want to add '{0}' to your watchlist".format(new_movie)
+            self.redirect("/?error=" + cgi.escape(error))
 
-
-        # TODO 1
         # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+        movie_escaped = cgi.escape(new_movie, quote=True)
 
         # build response content
-        new_movie_element = "<strong>" + new_movie + "</strong>"
+        new_movie_element = "<strong>" + movie_escaped + "</strong>"
         sentence = new_movie_element + " has been added to your Watchlist!"
         response = page_header + "<p>" + sentence + "</p>" + page_footer
         self.response.write(response)
