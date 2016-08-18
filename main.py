ee9c31ca7711649dcc79ab704f9e406c473c7f81
jesseilev <jesseilev@gmail.com>
2016-08-18 11:21:19 -0500

    convert most stuff ot templates

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 import webapp2
 import cgi
+import jinja2
+import os
 
-# html boilerplate for the top of every page
-page_header = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>FlickList</title>
-    <style type="text/css">
-        .error {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <h1>
-        <a href="/">FlickList</a>
-    </h1>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
-# html boilerplate for the bottom of every page
-page_footer = """
-</body>
-</html>
-"""
+
+# load some templates
+t_page_header = jinja_env.get_template("page-header.html")
+t_page_footer = jinja_env.get_template("page-footer.html")
 
 
 # a list of movies that nobody should be allowed to watch
@@ -48,46 +34,14 @@ class Index(webapp2.RequestHandler):
     """
 
     def get(self):
+        t_edit = jinja_env.get_template("edit.html")
 
-        edit_header = "<h3>Edit My Watchlist</h3>"
+        main_content = t_edit.render(
+                        watchlist = getCurrentWatchlist(),
+                        error = self.request.get("error"))
+        page_header = t_page_header.render(title="FlickList")
+        page_footer = t_page_footer.render()
 
-        # a form for adding new movies
-        add_form = """
-        <form action="/add" method="post">
-            <label>
-                I want to add
-                <input type="text" name="new-movie"/>
-                to my watchlist.
-            </label>
-            <input type="submit" value="Add It"/>
-        </form>
-        """
-
-        # a form for crossing off movies
-        # (first we build a dropdown from the current watchlist items)
-        crossoff_options = ""
-        for movie in getCurrentWatchlist():
-            crossoff_options += '<option value="{0}">{0}</option>'.format(movie)
-
-        crossoff_form = """
-        <form action="/cross-off" method="post">
-            <label>
-                I want to cross off
-                <select name="crossed-off-movie"/>
-                    {0}
-                </select>
-                from my watchlist.
-            </label>
-            <input type="submit" value="Cross It Off"/>
-        </form>
-        """.format(crossoff_options)
-
-        # if we have an error, make a <p> to display it
-        error = self.request.get("error")
-        error_element = "<p class='error'>" + error + "</p>" if error else ""
-
-        # combine all the pieces to build the content of our response
-        main_content = edit_header + add_form + crossoff_form + error_element
         response = page_header + main_content + page_footer
         self.response.write(response)
 
@@ -98,7 +52,6 @@ class AddMovie(webapp2.RequestHandler):
     """
 
     def post(self):
-        # look inside the request to figure out what the user typed
         new_movie = self.request.get("new-movie")
 
         # if the user typed nothing at all, redirect and yell at them
@@ -111,14 +64,13 @@ class AddMovie(webapp2.RequestHandler):
             error = "Trust me, you don't want to add '{0}' to your Watchlist.".format(new_movie)
             self.redirect("/?error=" + cgi.escape(error))
 
-        # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+        # escape the user's input so that if they typed HTML, it doesn't mess up our response
         movie_escaped = cgi.escape(new_movie, quote=True)
 
-        # build response content
-        new_movie_element = "<strong>" + movie_escaped + "</strong>"
-        sentence = new_movie_element + " has been added to your Watchlist!"
-        response = page_header + "<p>" + sentence + "</p>" + page_footer
-        self.response.write(response)
+        # TODO 1
+        # Use a template to render the confirmation message
+
+        self.response.write("Under construction...")
 
 
 class CrossOffMovie(webapp2.RequestHandler):
@@ -127,13 +79,14 @@ class CrossOffMovie(webapp2.RequestHandler):
     """
 
     def post(self):
-        # look inside the request to figure out what the user typed
         crossed_off_movie = self.request.get("crossed-off-movie")
 
-        if (crossed_off_movie in getCurrentWatchlist()) == False:
-            # the user tried to cross off a movie that isn't in their list,
-            # so we redirect back to the front page and yell at them
+        if not crossed_off_movie or crossed_off_movie.strip() == "":
+            error = "Please specify a movie to cross off."
+            self.redirect("/?error=", cgi.escape(error))
 
+        # if user tried to cross off a movie that is not in their list, reject
+        if not (crossed_off_movie in getCurrentWatchlist()):
             # make a helpful error message
             error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
             error_escaped = cgi.escape(error, quote=True)
@@ -141,10 +94,15 @@ class CrossOffMovie(webapp2.RequestHandler):
             # redirect to homepage, and include error as a query parameter in the URL
             self.redirect("/?error=" + error_escaped)
 
-        # if we didn't redirect by now, then all is well
-        crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
-        confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
-        response = page_header + "<p>" + confirmation + "</p>" + page_footer
+        # render confirmation page
+        t_cross_off = jinja_env.get_template("cross-off.html")
+        main_content = t_cross_off.render(movie=crossed_off_movie)
+        page_header = t_page_header.render(
+                        title="FlickList: " + crossed_off_movie
+                        + " has been crossed off your Watchlist")
+        page_footer = t_page_footer.render()
+
+        response = page_header + main_content + page_footer
         self.response.write(response)
 
 
