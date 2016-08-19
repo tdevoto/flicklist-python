65655d6275ac2fa0fc9034087fea966331a8a86b
jesseilev <jesseilev@gmail.com>
2016-08-18 22:14:27 -0500

    starter code is ready

--- main.py
+++ main.py
@@ -57,17 +57,17 @@ class AddMovie(webapp2.RequestHandler):
         new_movie = self.request.get("new-movie")
 
         # if the user typed nothing at all, redirect and yell at them
-        if not new_movie or new_movie.strip() == "":
+        if (not new_movie) or (new_movie.strip() == ""):
             error = "Please specify the movie you want to add."
             self.redirect("/?error=" + cgi.escape(error))
 
         # if the user wants to add a terrible movie, redirect and yell at them
         if new_movie in terrible_movies:
             error = "Trust me, you don't want to add '{0}' to your Watchlist.".format(new_movie)
-            self.redirect("/?error=" + cgi.escape(error))
+            self.redirect("/?error=" + cgi.escape(error, quote=True))
 
-        # escape the user's input so that if they typed HTML, it doesn't mess up our response
-        movie_escaped = cgi.escape(new_movie, quote=True)
+        # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+        new_movie_escaped = cgi.escape(new_movie, quote=True)
 
         # TODO 1
         # Use a template to render the confirmation message
