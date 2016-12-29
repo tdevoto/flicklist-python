702a6b61927a941feb3bc216cf536b62dc9735ee
jesseilev <jesseilev@gmail.com>
2016-12-29 10:38:56 -0600

    get rid of studio TODOs

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import webapp2
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
-
-        # TODO: make a list with at least 5 movie titles
-
-        # TODO: randomly choose one of the movies, and return it
-
+        # for now, just return a hard-coded string
         return "The Big Lebowski"
 
     def get(self):
@@ -16,9 +12,6 @@ class Index(webapp2.RequestHandler):
         # build the response string
         response = "<h1>Movie of the Day</h1>"
         response += "<p>" + movie + "</p>"
-
-        # TODO: pick a different random movie, and display it under
-        # the heading "<h1>Tommorrow's Movie</h1>"
 
         self.response.write(response)
 
