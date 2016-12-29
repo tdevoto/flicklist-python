07a91b4ec7df8f01d8aa121cbd30ee44a16910b9
jesseilev <jesseilev@gmail.com>
2016-12-29 15:50:26 -0600

    change variable name from response to content

--- main.py
+++ main.py
@@ -56,10 +56,10 @@ class Index(Handler):
     def get(self):
         unwatched_movies = db.GqlQuery("SELECT * FROM Movie where watched = False")
         t = jinja_env.get_template("frontpage.html")
-        response = t.render(
+        content = t.render(
                         movies = unwatched_movies,
                         error = self.request.get("error"))
-        self.response.write(response)
+        self.response.write(content)
 
 class AddMovie(Handler):
     """ Handles requests coming in to '/add'
@@ -88,8 +88,8 @@ class AddMovie(Handler):
 
         # render the confirmation message
         t = jinja_env.get_template("add-confirmation.html")
-        response = t.render(movie = movie)
-        self.response.write(response)
+        content = t.render(movie = movie)
+        self.response.write(content)
 
 
 class WatchedMovie(Handler):
@@ -118,8 +118,8 @@ class WatchedMovie(Handler):
 
         # render confirmation page
         t = jinja_env.get_template("watched-it-confirmation.html")
-        response = t.render(movie = watched_movie)
-        self.response.write(response)
+        content = t.render(movie = watched_movie)
+        self.response.write(content)
 
 
 class MovieRatings(Handler):
@@ -133,8 +133,8 @@ class MovieRatings(Handler):
         # in the query above, add something so that the movies are sorted by creation date, most recent first
 
         t = jinja_env.get_template("ratings.html")
-        response = t.render(movies = watched_movies)
-        self.response.write(response)
+        content = t.render(movies = watched_movies)
+        self.response.write(content)
 
     def post(self):
         rating = self.request.get("rating")
@@ -151,8 +151,8 @@ class MovieRatings(Handler):
 
             # render confirmation
             t = jinja_env.get_template("rating-confirmation.html")
-            response = t.render(movie = movie)
-            self.response.write(response)
+            content = t.render(movie = movie)
+            self.response.write(content)
         else:
             self.renderError(400)
 
