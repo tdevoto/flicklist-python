dfec1aab04d0ffc3665f403510e691cb6d858344
jesseilev <jesseilev@gmail.com>
2017-01-30 12:31:19 -0600

    escape error message before rendering

--- main.py
+++ main.py
@@ -48,9 +48,8 @@ class Index(Handler):
 
     def get(self):
         t = jinja_env.get_template("frontpage.html")
-        content = t.render(
-                        movies = getUnwatchedMovies(),
-                        error = self.request.get("error"))
+        error = cgi.escape(self.request.get("error"), quote=True)
+        content = t.render(movies=getUnwatchedMovies(), error=error)
         self.response.write(content)
 
 class AddMovie(Handler):
